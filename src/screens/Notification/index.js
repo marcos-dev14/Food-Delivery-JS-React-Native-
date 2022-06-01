414046872e03b7960bb69f5f5c8f5b420888ee9d
marcospaulodev <suporte@tgoo.pt>
2022-06-01 12:09:37 -0300

    First Commit

---
+++ src/screens/Notification/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export function Notification() {
+  return <View></View>;
+}
---
+++ src/screens/index.js
@@ -0,0 +1,11 @@
+import { MainLayout } from './MainLayout';
+
+import { Home } from './Home';
+
+import { Cart } from './Cart';
+
+import { Favorite } from './Favorite';
+
+import { Notification } from './Notification';
+
+export { MainLayout, Home, Cart, Favorite, Notification };
